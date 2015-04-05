264ef47d1b558842ca6761b24ae4a87834d036f8
Vincent Fiestada <vffiestada@upd.edu.ph>
2015-04-05 19:56:01 +0800

    Fully working prototype (extensive testing pending); FIFO algo used

--- dish.cpp
+++ dish.cpp
@@ -1,32 +1,130 @@
 #pragma once
 
 #include <string>
+#define COOK 0
+#define PREP 1
 
 using namespace std;
 
+enum dish_state {READY, PREPPING, ONSTOVE, NOTARRIVED, MOVING, DONE};
+
+class Task
+{
+public:
+    Task(int type, int duration)
+    {
+        _type = type; // COOK or PREP
+        _time = duration; // "seconds" remaining before task completion
+    }
+    int GetType() // getter of type property
+    {
+        return _type;
+    }
+    string GetStringType() // getter of type as string
+    {
+        return (_type == COOK) ? "Cook" : "Prep";
+    }
+    int GetTime() // getter for time property
+    {
+        return _time;
+    }
+    void Step() // execute 1 "second" of the task
+    {
+        _time--;
+    }
+    bool IsDone() // return true if the task is done
+    {
+        return (_time <= 0);
+    }
+private:
+    int _type;
+    int _time;
+};
+
 class Dish
 {
-    public:
-    Dish(string name, int arrival)
+public:
+    /*
+     * Constructor
+     */
+    Dish(string name, int arrival, int priority)
     {
-        Name = name; // MUST match filename for recipe
-        ArrivalTime = arrival;
-    }
-    // Getters and Setters
-    string GetName()
-    {
-        return Name;
-    }
-    void SetName(string n)
-    {
-        Name = n;
-    }
-    int GetArrival()
-    {
-        return ArrivalTime;
+        _name = name; // MUST match filename for recipe
+        _arrivalTime = arrival; // "second" at which this dish is due to arrive
+        _priority = priority; // priority of dish used for scheduling algorithm
+        _state = NOTARRIVED; // initial state is NotArrived always
     }
 
-    private:
-    string Name;
-    int ArrivalTime;
+    // Getters and Setters
+    string GetName() // getter for name
+    {
+        return _name;
+    }
+    void SetName(string n) // setter for name
+    {
+        _name = n;
+    }
+    int GetArrival() // getter for arrival time
+    {
+        return _arrivalTime;
+    }
+    int GetPriority() // getter for priority
+    {
+        return _priority;
+    }
+    void SetPriority(int p) // setter for priority
+    {
+        _priority = p;
+    }
+    dish_state GetState() // getter for state
+    {
+        return _state;
+    }
+    void SetState(dish_state s) // setter for state
+    {
+        _state = s;
+    }
+    vector<Task>& GetRecipe() // getter for recipe (returns reference)
+    {
+        return _recipe;
+    }
+    Task * GetNextTask() // get next unfinished task in recipe (reference)
+    {
+        for (int i = 0; i < _recipe.size(); i++)
+        {
+            // return the first task in recipe that is not yet done
+            if (_recipe[i].IsDone() == false)
+            {
+                return &(_recipe.at(i));
+            }
+        }
+        return NULL;
+    }
+    bool IsDone() // returns true if all tasks are done
+    {
+        // if all tasks are done, dish is done too
+        return (this->GetNextTask() == NULL) ? true : false;
+    }
+    friend ostream& operator << (ostream &out, Dish &d)
+    {
+        out << d.GetName() << "(";
+        Task * t = d.GetNextTask();
+        if (t)
+        {
+            out << t->GetStringType() << " - " << t->GetTime();
+        }
+        else
+        {
+            out << "Done";
+        }
+        out << ")";
+        return out;
+    }
+
+private:
+    string _name;
+    int _arrivalTime;
+    int _priority;
+    dish_state _state;
+    vector<Task> _recipe;
 };
--- scheduler.cpp
+++ scheduler.cpp
@@ -7,96 +7,247 @@
 using namespace std;
 
 #define INPUTFILE "tasklist.txt"
+#define OUTPUTFILE "output.csv"
+#define STOVE_DIRTY 0
+#define STOVE_CLEAN 2
+
+void fatal_err(const string &s, int code);
 
 class Scheduler
 {
 public:
+    /*
+     * Constructors
+     */
     Scheduler()
     {
-        OnStove = -1;
-        Time = 0;
+        _onStove = -1; // Nothing is on the stove
+        _time = 0; // Begin at 0 "seconds"/_time units
+        _stoveStatus = STOVE_CLEAN - 1;
     }
     Scheduler(const vector<Dish> &d)
     {
         for (int i = 0; i < d.size(); i++)
         {
-            Dishes.push_back(d.at(i)); // Copy d into Dishes
+            _dishes.push_back(d.at(i)); // Copy d into _dishes
         }
-        OnStove = -1;
-        Time = 0;
+        _onStove = -1; // Nothing is on the stove
+        _time = 0; // Begin at 0 "seconds"/_time units
+        _stoveStatus = STOVE_CLEAN - 1;
     }
-    vector<Dish>& GetDishes()
+
+    vector<Dish>& GetDishes() // getter for Dishes not yet arrived (reference)
     {
-        return Dishes;
+        return _dishes;
     }
-    vector<Dish>& GetReady()
+    Dish * GetOnStove() // getter for dish on stove (reference)
     {
-        return Ready;
+        return (_onStove > -1) ? &(_dishes.at(_onStove)) : NULL;
     }
-    vector<Dish>& GetPrep()
+    int GetTime() // getter for time
     {
-        return Prep;
+        return _time;
     }
-    int GetTime()
+    void Sim() // Begin simulation
     {
-        return Time;
-    }
-    void Sim() /* Begin simulation */
-    {
-        while (Dishes.size() > 0 || Ready.size() > 0 || Prep.size() > 0)
+        int n = _dishes.size();
+        ofstream out(OUTPUTFILE, ofstream::out);
+        if (out.is_open())
         {
-            Proceed();
+            // Print CSV headers
+            out << "Time, Stove, Ready, Assistants, Remarks" << endl;
+            while (n > 0)
+            {
+                n = Proceed(n, out);
+            }
+            out.close();
+        }
+        else
+        {
+            fatal_err("Output file could not be opened.", 6);
         }
     }
 private:
-    void Proceed()
+    /* Schedule() - selects the next dish to be cooked
+     *            - no arguments
+     *            - returns index (in _dishes) of dish to be cooked
+     */
+    int Schedule()
     {
-        Time++;
-        cout << "Time: " << Time << endl;
-        /**** Check if a dish is arriving ****/
-        for (int i = 0; i < Dishes.size(); i++)
+        int k = 0;
+        while (_dishes.at(k).GetState() != READY && _dishes.at(k).GetState() != ONSTOVE)
         {
-            if (Dishes.at(i).GetArrival() == Time)
+            k++;
+            if (k == _dishes.size())
             {
-                cout << Dishes.front().GetName() << " arrives" << endl;
-                Ready.push_back(Dishes.front());
-                Dishes.erase(Dishes.begin() + i);
+                k = -1;
+                break;
             }
         }
-        /**** Sort Ready Queue ****/
+        return k;
+    }
+    /* Proceed() - moves the simulation one step forward in time
+     *           - arguments are number of Dishes not done & output stream
+     *           - returns number of Dishes not done;
+     */
+    int Proceed(int n, ostream &out) // argument : output stream where output is to be printed
+    {
+        string remarks; // Stores remarks string
+        _time++; // Time travel (1 second ahead)
+        /**** Check if a dish is arriving ****/
+        for (int i = 0; i < _dishes.size(); i++)
+        {
+            // A Dish has "arrived" if its arrival time is equal to current "time"
+            if (_dishes.at(i).GetArrival() == _time)
+            {
+                // Add to Remarks
+                remarks += _dishes.at(i).GetName() + " arrives. ";
+
+                Dish &d = _dishes.at(i);
+                Task * t = d.GetNextTask();
+                if (t->GetType() == COOK)
+                {
+                    d.SetState(READY); // Ready for cooking
+                }
+                else
+                {
+                    d.SetState(PREPPING); // Must be prepped
+                }
+            }
+        }
+        /**** Select which one to cook next ****/
+
+        // TESTING >>=>> Use First Come First Serve
+        int k;
+        if (_onStove > -1 && _onStove < _dishes.size() && _dishes.at(_onStove).GetState() == READY)
+        {
+            k = _onStove;
+        }
+        else
+        {
+            k = Schedule();
+        }
+
+        // if Stove is clean or dish isn't changed, proceed normally
+        if (k == _onStove || _stoveStatus == STOVE_CLEAN)
+        {
+            _onStove = k;
+            // The stove is "dirty" now iff. it is not empty
+            if (_onStove > -1) _stoveStatus = STOVE_DIRTY;
+        }
+        // Stove is dirty and dish needs to be changed
+        else if (_stoveStatus == STOVE_DIRTY)
+        {
+            _onStove = -1;
+            remarks += "Cleaning stove. ";
+            _stoveStatus += 1; // 1 step towards a clean stove
+        }
+        // Dish needs to be changed and stove is almost clean
+        else
+        {
+            _onStove = -1;
+            remarks += "Preheating stove. ";
+            _stoveStatus += 1; // Stove is clean next time
+        }
+
+        if (_onStove > -1)
+        {
+            _dishes.at(_onStove).SetState(ONSTOVE);
+        }
+
         /**** Cook ****/
 
-        cout << "Ready: ";
-        for (int j = 0; j < Ready.size(); j++)
-        {
-            cout << Ready.at(j).GetName() << ' ';
-        }
-        cout << endl;
+        // Print Time
+        out << _time << ", ";
 
-        OnStove = 0;
-        if (Ready.size() >= 1)
+        // Print Dish on stove
+        if (_onStove > -1)
         {
-            cout << "OnStove: " << Ready.at(OnStove).GetName() << endl;
+            out << _dishes.at(_onStove);
         }
+        else
+        {
+            out << "-- Idle --";
+        }
+        out << ", ";
+        // Print Ready
+        for (int i = 0; i < _dishes.size(); i++)
+        {
+            if (_dishes.at(i).GetState() == READY && _dishes.at(i).GetState() != ONSTOVE)
+            {
+                out << _dishes.at(i) << "   ";
+            }
+        }
+        out << ", ";
+        // Print Assistants/Prep
+        for (int i = 0; i < _dishes.size(); i++)
+        {
+            if (_dishes.at(i).GetState() == PREPPING)
+            {
+                out << _dishes.at(i) << "   ";
+            }
+        }
+        out << ", ";
 
-        Ready.clear();
+        for (int i = 0; i < _dishes.size(); i++)
+        {
+            if (_dishes.at(i).GetState() == NOTARRIVED || _dishes.at(i).GetState() == DONE)
+            {
+                continue;
+            }
+
+            Task * t = _dishes.at(i).GetNextTask();
+
+            // Do Tasks
+            dish_state dS = _dishes.at(i).GetState();
+            if (dS == ONSTOVE || dS == PREPPING)
+            {
+                t->Step();
+            }
+            t = _dishes.at(i).GetNextTask();
+
+            // Set States based on next task
+            if (t == NULL)
+            {
+                // No more tasks in recipe -- dish is done
+                remarks += _dishes.at(i).GetName() + " is Done. ";
+                //_dishes.erase(_dishes.begin() + i);
+                _dishes.at(i).SetState(DONE);
+                n -= 1; // decrease number of dishes not done yet
+            }
+            else if (t->GetType() == COOK)
+            {
+                _dishes.at(i).SetState(READY);
+            }
+            else if (t->GetType() == PREP)
+            {
+                if (dS == ONSTOVE)
+                {
+                    _dishes.at(i).SetState(MOVING);
+                }
+                else
+                {
+                    _dishes.at(i).SetState(PREPPING);
+                }
+            }
+        }
+        // Print Remarks
+        out << remarks << endl;
+
+        /*** Return number of dishes not yet done ***/
+        return n;
     }
-    vector<Dish> Dishes;
-    vector<Dish> Ready;
-    vector<Dish> Prep;
-    int OnStove; // Index in Ready of Dish currently On Stove
-    int Time;
+    vector<Dish> _dishes;
+    int _onStove; // Index in _dishes of Dish currently On Stove
+    int _time;
+    int _stoveStatus; // Stove status (from DIRTY to CLEAN, with 1 step in between)
 };
-
-void fatal_err(const string &s, int code);
 
 int main()
 {
     cout << endl << "CS 140 Machine Problem" << endl;
     cout << "----------------------------" << endl;
     cout << "Vincent Fiestada | 201369155" << endl << endl;
-
-    cout << "Dishes to Prepare:" << endl;
 
     Scheduler core = Scheduler();
 
@@ -113,13 +264,63 @@ int main()
             {
                 // The name is the substr before the space
                 // The number is the substr after the space
-                int at = atoi(taskDesc.substr(x + 1).c_str());
+                int at = atoi(taskDesc.substr(x + 1).c_str()); // convert string to int
                 if (at == 0)
                 {
                     input.close();
-                    fatal_err("Input file is corrupted. Invalid arrival time.", 3);
+                    fatal_err("Input file is corrupted. Invalid arrival _time.", 3);
                 }
-                core.GetDishes().push_back(Dish(taskDesc.substr(0, x), at));
+                Dish d = Dish(taskDesc.substr(0, x), at, 0);
+                // Open recipe file and add recipe steps to dish d
+                string recipeFilename = "recipes/" + d.GetName() + ".txt";
+                ifstream recipeFile(recipeFilename.c_str());
+                if (recipeFile.is_open())
+                {
+                    string recipeLine;
+                    size_t y;
+                    if (getline(recipeFile, recipeLine))
+                    {
+                        // Parse by finding the space
+                        y = recipeLine.find(" ");
+                        if (y != string::npos)
+                        {
+                            // The priority is the number after the space
+                            int p = atoi(recipeLine.substr(y + 1).c_str()); // convert string to int
+                            d.SetPriority(p);
+                        }
+                        else
+                        {
+                            // space not found, trigger error
+                            fatal_err("Recipe file '" + recipeFilename + "' is corrupted. Dish priority is missing", 5);
+                        }
+                    }
+                    while(getline(recipeFile, recipeLine))
+                    {
+                        // Parse by finding the space
+                        y = recipeLine.find(" ");
+                        if (y != string::npos)
+                        {
+                            // Step description is before the space
+                            int ty = (recipeLine.substr(0, y) == "cook") ? COOK : PREP;
+                            // Step time is the number after the space
+                            int ti = atoi(recipeLine.substr(y + 1).c_str()); // convert string to int
+                            d.GetRecipe().push_back(Task(ty, ti)); // Push into recipe vector
+                        }
+                        else
+                        {
+                            // space not found, trigger error
+                            fatal_err("Recipe file '" + recipeFilename + "' is corrupted.", 5);
+                        }
+                    }
+                    recipeFile.close();
+
+                    // push into dishes
+                    core.GetDishes().push_back(d);
+                }
+                else
+                {
+                    fatal_err("Recipe file '" + recipeFilename + "' not found.", 4);
+                }
             }
             else
             {
